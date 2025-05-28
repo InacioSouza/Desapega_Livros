a77246a7667fa0b29aeeaa6cc21b0edecf37a0e5
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2025-05-27 21:22:18 -0300

    Personalização da página swagger-ui

--- back/src/main/java/br/com/livraria/desapega_livros/infra/springdoc/SpringDocConfigurations.java
+++ back/src/main/java/br/com/livraria/desapega_livros/infra/springdoc/SpringDocConfigurations.java
@@ -2,6 +2,8 @@ package br.com.livraria.desapega_livros.infra.springdoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +13,19 @@ public class SpringDocConfigurations {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI()
+        var openAPI = new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
                                 new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+
+        openAPI.info(new Info().title("Desapega Livros API")
+                .description("Documentação da API de livraria compartilhada - Desapega Livros")
+                .version("1.0")
+                .contact(new Contact()
+                        .name("INÁCIO SOUZA ROCHA")
+                        .url("https://www.linkedin.com/in/inacio-souza/")));
+
+        return openAPI;
     }
 
 
